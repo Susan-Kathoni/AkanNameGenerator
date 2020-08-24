ec99216eca6850dd4b6d90672db422d07b978b1a
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-08-24 01:12:36 -0400

    Adding functions to execute input made by the user to output Akan name and day of the week.

---
+++ akan-name.js
@@ -0,0 +1,51 @@
+var dd="Sunday":"", Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"
+var maleNames="Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"
+var femaleNames="Akosua:, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"
+var mm=[""Jan":"0", "Feb":"1", "Mar": "02", "Apr": "03", "May":"04", "June":"05", "July":"06", "Aug":"07", "Sept":"08", "Oct":"9", "Nov":"10", "Dec":"11"] ; 
+
+
+function myFunction () {
+    document.getElementById("mm").style.caretColor="yellow";
+}
+
+function myFunction() {
+    var dd = birthday Date();
+    var day = dd Array (7);
+    dd[0] = "Sunday"
+    dd[1] = "Monday"
+    dd[2] = "Tuesday"
+    dd[3] = "Wednesday"
+    dd[4] = "Thursday"
+    dd[5] = "Friday"
+    dd[6] = "Saturday"
+
+    var x = dd[dd.getDay()];
+    document.getElementById("dd").innerHTML = days[dd.getDay()];
+}
+
+function myFunction() {
+    var femaleNames = femaleNames name();
+    var femaleNames = femaleNames Array(7);
+    femaleName[0] = "Kwasi";
+    femaleName[1] = "Kwadwo";
+    femaleName[2] = "Abenaa";
+    femaleName[3] = "Akua";
+    femaleName[4] = "Yaa";
+    femaleName[5] = "Afua";
+    femaleName[6] = "Ama";
+
+    var femaleNames = femaleNames[femaleNames.getName()];
+    document.getElementById("gridRadios2")
+}
+
+function myFunction() {
+    var maleNames = maleNames name();
+    var fmaleNames = maleNames Array(7);
+    maleName[0] = "Akosua";
+    maleName[1] = "Adwoa";
+    maleName[2] = "Kwabena";
+    maleName[3] = "Kwaku";
+    maleName[4] = "Yaw";
+    maleName[5] = "Kofi";
+    maleName[6] = "Kwame";
+}
