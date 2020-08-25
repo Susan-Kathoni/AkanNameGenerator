2284a3943b374df4bad73ccd97a28a9e76764210
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-08-25 19:30:17 -0400

    add Js

--- akan-name.js
+++ akan-name.js
@@ -1,51 +1,54 @@
-var dd="Sunday":"", Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"
-var maleNames="Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"
-var femaleNames="Akosua:, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"
-var mm=[""Jan":"0", "Feb":"1", "Mar": "02", "Apr": "03", "May":"04", "June":"05", "July":"06", "Aug":"07", "Sept":"08", "Oct":"9", "Nov":"10", "Dec":"11"] ; 
-
-
-function myFunction () {
-    document.getElementById("mm").style.caretColor="yellow";
+function test() {
+  alert("this is a test");
 }
-
 function myFunction() {
-    var dd = birthday Date();
-    var day = dd Array (7);
-    dd[0] = "Sunday"
-    dd[1] = "Monday"
-    dd[2] = "Tuesday"
-    dd[3] = "Wednesday"
-    dd[4] = "Thursday"
-    dd[5] = "Friday"
-    dd[6] = "Saturday"
+  var genders = document.getElementById("my-dropdown").selectedIndex;
 
-    var x = dd[dd.getDay()];
-    document.getElementById("dd").innerHTML = days[dd.getDay()];
-}
+  const ourDate = "";
+  let month = document.getElementById("month").value;
+  let day = document.getElementById("day").value;
+  let year = document.getElementById("year").value;
 
-function myFunction() {
-    var femaleNames = femaleNames name();
-    var femaleNames = femaleNames Array(7);
-    femaleName[0] = "Kwasi";
-    femaleName[1] = "Kwadwo";
-    femaleName[2] = "Abenaa";
-    femaleName[3] = "Akua";
-    femaleName[4] = "Yaa";
-    femaleName[5] = "Afua";
-    femaleName[6] = "Ama";
+  let userAkan;
+  var d = new Date(ourDate.concat(month, " ", day, " ", year));
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
 
-    var femaleNames = femaleNames[femaleNames.getName()];
-    document.getElementById("gridRadios2")
-}
-
-function myFunction() {
-    var maleNames = maleNames name();
-    var fmaleNames = maleNames Array(7);
-    maleName[0] = "Akosua";
-    maleName[1] = "Adwoa";
-    maleName[2] = "Kwabena";
-    maleName[3] = "Kwaku";
-    maleName[4] = "Yaw";
-    maleName[5] = "Kofi";
-    maleName[6] = "Kwame";
+  var n = weekday[d.getDay()];
+  console.log(n);
+  var days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  var maleName = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+  const findDay = (element) => element == n;
+  var dayIndex = days.findIndex(findDay);
+  if (genders === 1) {
+    userAkan = maleName[dayIndex];
+  } else if (genders === 2) {
+    userAkan = femaleName[dayIndex];
+  }
+  console.log(dayIndex, femaleName[3]);
+  alert(userAkan);
+  return userAkan;
 }
